e64532213828efa33996268c414fd2ea95846ec5
mrZalli <misterzalli@gmail.com>
2016-08-29 15:12:21 +0300

    Removed Gruntfile and modified task. Gruntfile contained a password that has now been changed.

--- src/task.js
+++ src/task.js
@@ -1,5 +1,6 @@
 
-const task_types = {
+const tasks = {
+    IDLE: 'idle',
     HARVEST: 'harvest',
     STORE_ENERGY: 'store_e',
     BUILD: 'build',
@@ -11,6 +12,31 @@ const task_types = {
     DEFEND: 'defend',
     ATTACK: 'attack',
 };
+
+
+function do_task(creep, task, target) {
+    // get the task from the memory if not given as an argument
+    if (task === undefined) {
+        task = creep.memory.task;
+    }
+    // same with task
+    if (target === undefined) {
+        target = creep.memory.task_target;
+    }
+    switch (task) {
+        // we are harvesting energy
+        // target has to be an energy source
+        case tasks.HARVEST:
+            
+        break;
+
+        // if task is idle or nothing, do nothing
+        case tasks.IDLE:
+        default:
+
+    }
+}
+
 
 //module.exports.task = task;
 module.exports.task_types = task_types;
